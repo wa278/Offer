1941ba3b6f6f69ba7edbc9c282fbc20a4528dce3
wa278 <wa941121@163.com>
2018-11-10 20:27:55 +0800

    finish 43

---
+++ src/Solution43.java
@@ -0,0 +1,20 @@
+/*
+翻转单词顺序
+ */
+public class Solution43 {
+    public String ReverseSentence(String str) {
+        if(str.trim().equals("")){
+            return str;
+        }
+        String[] a = str.split(" ");
+        StringBuffer o = new StringBuffer();
+        int i;
+        for (i = a.length; i >0;i--){
+            o.append(a[i-1]);
+            if(i > 1){
+                o.append(" ");
+            }
+        }
+        return o.toString();
+    }
+}
