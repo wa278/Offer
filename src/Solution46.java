84a70f35d9358e21b92db0abd4d91b04487cc4fa
wa278 <wa941121@163.com>
2018-11-11 19:43:55 +0800

    solution 46

--- src/Solution46.java
+++ src/Solution46.java
@@ -4,6 +4,8 @@
  */
 public class Solution46 {
     public int Sum_Solution(int n) {
-        return n*(n+1)/2;
+        int ans = n;
+        boolean flag = ans >0 && (ans += Sum_Solution(n-1)) >0;
+        return ans;
     }
 }
